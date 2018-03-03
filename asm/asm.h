191a395bcf92dc5f14de825ebd76f9e1dd493f1a
OB42 <19638906+OB42@users.noreply.github.com>
2018-03-03 22:17:03 +0100

    Update asm.h

--- asm/asm.h
+++ asm/asm.h
@@ -19,15 +19,15 @@
 # include <sys/types.h>
 # include <sys/stat.h>
 # include <fcntl.h>
-# define ERR_USAGE				"Usage: ./asm <sourcefile.s>\n"
-# define ERR_FILE_READING		"Can't read source file\n"
-# define ERR_FILE_CREATION		"Can't create champion\n"
-# define ERR_MALLOC				"malloc returned 0\n"
-# define ERR_PARSING			"Parsing error\n"
-# define ERR_GNL				"GNL returned 0 or -1\n"
-# define ERR_COMMAND_LENGTH		"A command line is too long\n"
-# define ERR_INVALID_COMMAND	"A command is invalid\n"
-# define ERR_WRONG_EXTENSION	"The filename extension must be .s\n"
+# define ERR_USAGE "Usage: ./asm <sourcefile.s>\n"
+# define ERR_FILE_READING "Can't read source file\n"
+# define ERR_FILE_CREATION "Can't create champion\n"
+# define ERR_MALLOC "malloc returned 0\n"
+# define ERR_PARSING "Parsing error\n"
+# define ERR_GNL "GNL returned 0 or -1\n"
+# define ERR_COMMAND_LENGTH "A command line is too long\n"
+# define ERR_INVALID_COMMAND "A command is invalid\n"
+# define ERR_WRONG_EXTENSION "The filename extension must be .s\n"
 # define INPUT_EXTENSION ".s"
 # define OUTPUT_EXTENSION ".cor"
 
