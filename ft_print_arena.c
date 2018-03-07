06c4898f4978d7964753599fb33bdc6a92ff83f7
Marvin LEGEAY <mlegeay@e1r3p6.42.fr>
2018-03-07 17:26:55 +0100

    print en couleur

--- ft_print_arena.c
+++ ft_print_arena.c
@@ -6,20 +6,33 @@
 /*   By: mlegeay <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/07 14:37:46 by mlegeay           #+#    #+#             */
-/*   Updated: 2018/03/07 16:19:26 by mlegeay          ###   ########.fr       */
+/*   Updated: 2018/03/07 17:24:04 by mlegeay          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "op.h"
 
-void    ft_print_arena(unsigned char	*arena)
+void    ft_print_arena(unsigned char *arena, t_corewar *corewar)
 {
 	unsigned int i;
 
 	i = 0;
 	while (i < MEM_SIZE)
 	{
-		ft_printf("%02x ", arena[i]);
+		if (corewar->arena_id[i] == 48)
+			ft_printf("%s%02x ", KRED, arena[i]);
+		else if (corewar->arena_id[i] == 49)
+			ft_printf("%s%02x ", KGRN, arena[i]);
+		else if (corewar->arena_id[i] == 50)
+			ft_printf("%s%02x ", KYEL, arena[i]);
+		else if (corewar->arena_id[i] == 51)
+			ft_printf("%s%02x ", KBLU, arena[i]);
+		else if (corewar->arena_id[i] == 52)
+			ft_printf("%s%02x ", KMAG, arena[i]);
+		else
+		{
+			ft_printf("%s%02x ", KWHT, arena[i]);
+		}
 		i++;
 		if ((i % 16) == 0)
 			ft_printf("\n");
