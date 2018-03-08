2fe70ea12b70dc87e65b46c459f4096d369ac563
Otmane BENAZZOU <obenazzo@e2r8p15.42.fr>
2018-03-08 17:47:09 +0100

    improving tests

--- asm/unit_tests.js
+++ asm/unit_tests.js
@@ -1,46 +1,105 @@
-const exec = require('child_process').execSync;
+const {exec} = require('child_process');
+
 path = require("path");
-try {
-	exec(`rm vm_champs/*.cor`);
-} catch (e) {
-	console.log("nothing to del");
-}
-try {
-	exec(`rm mine/*.cor`);
-} catch (e) {
-	console.log("nothing to del");
-}
-exec(`make`);
-(fs = require('fs'))
-.readdirSync('./mine')
-.forEach((filename, i) => {
-	var b = path.resolve(`mine/${filename}`);
-	var a = path.resolve(`vm_champs/${filename}`);
-	var bx = path.resolve(`mine/${filename.replace('.s', '.cor')}`);
-	var ax = path.resolve(`vm_champs/${filename.replace('.s', '.cor')}`);
-	console.log('______________________')
-	try {
-		exec(`./asm ${a}`);
-	} catch (e) {
-//		console.log(e.stderr.toString());
+exec('rm vm_champs/*.cor;rm mine/*.cor;rm temp_test;make', () => {
+	var P = 0;
+	var F = 0;
+	var arr = (fs = require('fs'))
+	.readdirSync('./mine');
+	arr.forEach((filename, i) => {
+		var a = path.resolve(`mine/${filename}`).slice(0, -2);
+		var b = path.resolve(`vm_champs/${filename}`).slice(0, -2);
+		exec(`./asm ${a}.s`, (err, stdout, stderr) => {
+			exec(`./rasm ${b}.s`, (berr, bstdout, bstderr) => {
+				var ar = stdout + stderr;
+				var br = bstdout + bstderr;
+				if (ar.match("Writing output") && br.match("Writing output"))
+				{
+						exec(`diff ${a}.cor ${b}.cor`, (err, stdout, stderr) => {
+							if (!(stdout.length))
+							{
+								console.log('_________________________');
+								console.log("Both files were created and are identical.");
+								pass(filename, i);
+							}
+							else {
+								exec(`hexdump ${a}.cor > temp_hex_mine; hexdump ${b}.cor > temp_hex_vm_champs; diff temp_hex_mine temp_hex_vm_champs`, (err, stdout, stderr) => {
+									console.log('_________________________');
+									console.log(err + stderr + stdout)
+									fail(filename, i);
+								});
+							}
+						});
+				}
+				else if (!(ar.match("Writing output")) && !(br.match("Writing output")) && !(ar.match(/fault|bus|free/i)))
+				{
+					console.log('_________________________');
+					console.log("Both ASMs returned an error");
+					console.log("Mine:", ar, "vm_champs:", br);
+					pass(filename, i);
+				}
+				else if (ar.match(/fault|bus|free/i))
+				{
+					console.log('_________________________');
+					console.log("My ASM probably crashed", ar)
+					fail(filename, i);
+				}
+				else if (!(ar.match("Writing output")) && (br.match("Writing output")))
+				{
+					exec(`./rcorewar ${b}.cor`, (err, stdout, stderr) => {
+						console.log('_________________________');
+						console.log(stdout + stderr)
+						if ((stderr + stdout).match(/error/i))
+						{
+							console.log("Corewar and my ASM both returned an error.")
+							pass(filename, i);
+						}
+						else {
+							console.log("My ASM returned an error when compiling a valid file.")
+							console.log(ar);
+							fail(filename, i);
+						}
+					})
+				}
+				else {
+					console.log("THIS PROBABLY WONT HAPPEN(too lazy to handle this case)")
+				}
+			});
+		});
+	});
+	var fail = (filename) =>
+	{
+		console.log(`\x1b[31mFAILING ${F++ +1}/${arr.length} tests\x1b[0m`, filename);
 	}
-	try {
-		exec(`./rasm ${b}`);
-	} catch (e) {
-		console.log("error in vm_champs:", e.stdout.toString());
+	var pass = (filename) =>
+	{
+		console.log(`\x1b[32mPASSING ${P++ +1}/${arr.length} tests\x1b[0m`, filename);
 	}
-	try {
-		if ((fs.existsSync(ax) && fs.existsSync(bx)))
+})
+
+/*	try {
+		exec(`./rasm ${b}.s >> temp_test 2>&1`);
+	} catch (e) {
+		exec(`./rasm ${b}.s >> temp_test 2>&1`);
+	} finally {
+		if ((fs.existsSync(a + '.cor') && fs.existsSync(b + '.cor')))
 		{
-			exec(`diff ${ax} ${bx}`);
-			console.log(`${ax} and ${bx} are identical.`)
+//			exec(`diff ${ax} ${bx}`);
+			console.log(`good`)
 		}
-		if ((fs.existsSync(ax) && fs.existsSync(bx)) || (!fs.existsSync(ax) && !fs.existsSync(bx)))
+		else {
+			console.log(`bad`)
+		}
+
+	}*/
+
+/*		if ((fs.existsSync(ax) && fs.existsSync(bx)) || (!fs.existsSync(ax) && !fs.existsSync(bx)))
 			console.log('\x1b[32mPASSING\x1b[0m', filename , `Both files were ${(fs.existsSync(ax) && fs.existsSync(bx)) ? '' : 'NOT '}created`);
 		else
 		{
 			try {
-				exec(`./rcorewar ${bx}`);
+				console.log("COREWAR", filename)
+				exec(`./rcorewar ${ax}`);
 				console.log('\x1b[31mFAILING\x1b[0m', filename);
 
 			} catch (e) {
@@ -58,5 +117,4 @@ exec(`make`);
 			console.log(e.stdout.toString());
 			console.log('\x1b[31mDIFFER\x1b[0m', filename);
 		}
-	}
-});
+	}*/
