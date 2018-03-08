4fffa594152f63ce6de6c9cacd81f361057c66b0
Otmane BENAZZOU <obenazzo@e2r8p15.42.fr>
2018-03-08 15:25:28 +0100

    unit_tests.js

---
+++ asm/unit_tests.js
@@ -0,0 +1,85 @@
+const exec = require('child_process').execSync;
+path = require("path");
+try {
+	exec(`rm vm_champs/*.cor`);
+} catch (e) {
+	console.log("nothing to del");
+}
+try {
+	exec(`rm mine/*.cor`);
+} catch (e) {
+	console.log("nothing to del");
+}
+exec(`make`);
+(fs = require('fs'))
+.readdirSync('./mine')
+.forEach((filename, i) => {
+	var b = path.resolve(`mine/${filename}`);
+	var a = path.resolve(`vm_champs/${filename}`);
+	var bx = path.resolve(`mine/${filename.replace('.s', '.cor')}`);
+	var ax = path.resolve(`vm_champs/${filename.replace('.s', '.cor')}`);
+	console.log('______________________')
+	try {
+		exec(`./asm ${a}`);
+	} catch (e) {
+//		console.log(e.stderr.toString());
+	}
+	try {
+		exec(`./rasm ${b}`);
+	} catch (e) {
+		console.log("error in vm_champs:", e.stdout.toString());
+//		console.log("err in vm_champs asm", filename);
+	}
+	try {
+		if ((fs.existsSync(ax) && fs.existsSync(bx)))
+		{
+			exec(`diff ${ax} ${bx}`);
+			console.log(`${ax} and ${bx} are identical.`)
+		}
+		if ((fs.existsSync(ax) && fs.existsSync(bx)) || (!fs.existsSync(ax) && !fs.existsSync(bx)))
+			console.log('\x1b[32mPASSING\x1b[0m', filename , `Both files were ${(fs.existsSync(ax) && fs.existsSync(bx)) ? '' : 'NOT '}created`);
+		else
+		{
+			try {
+				exec(`./rcorewar ${bx}`);
+				console.log('\x1b[31mFAILING\x1b[0m', filename);
+
+			} catch (e) {
+				console.log("error in corewar:", e.stdout.toString());
+				console.log('\x1b[32mPASSING\x1b[0m', filename);
+			}
+		}
+	} catch (e) {
+		exec(`hexdump ${bx} > temp_hex_mine`);
+		exec(`hexdump ${ax} > temp_hex_vm_champs`);
+		try {
+			exec(`diff temp_hex_mine temp_vm_champs`);
+		}
+		catch (e){
+			console.log(e.stdout.toString());
+			console.log('\x1b[31mDIFFER\x1b[0m', filename);
+		}
+	}
+
+
+});
+
+
+/*
+//>> log_file 2>&1
+
+var p = path.resolve(`ch/${filename}`);
+
+try {
+	exec(`./rcorewar ${p}`);
+
+} catch (e) {
+	console.log("err", filename);
+}
+if (fs.existsSync(`./ch/${filename.replace('.s', '.cor')}`))
+{
+	console.log("yeah", filename);
+}
+
+//if (output.slice(0,5) != 'ERROR')
+	//console.log(filename);*/
