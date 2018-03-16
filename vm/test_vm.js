f5119d4a5591e698bf8a6ab8a654091ebaa40dfa
OB42 <19638906+OB42@users.noreply.github.com>
2018-03-16 23:32:00 +0100

    Update test_vm.js

--- vm/test_vm.js
+++ vm/test_vm.js
@@ -15,7 +15,7 @@ path = require("path");
 var F = 0;
 var P = 0;
 var show_msg = (arr) => {
-	console.log('_______________________________________________________________');
+	console.log('_____________________________________________________________________________________________________________________________');
 	arr.forEach(x => console.log(x));
 }
 var fail = (arr, filename, id) =>
@@ -89,99 +89,17 @@ var test = (files, err, stdout, stderr, callback) => {
 		pass(msg_arr, `${files[0]} vs ${files[1]}`, files[2]);
 	callback();
 };
-exec(`make; rm errors/*; make -C ../asm; ${c.map(x => `../asm/asm ../asm/mine/${x.replace('.cor', '.s')}`).join(';')}; make fclean -C ../asm`, () => {
-	async.eachLimit(t, 4, function(files, callback) {
-		exec(`./corewar ../asm/mine/${files[0]} ../asm/mine/${files[1]}`,
-		(err, stdout, stderr) => {
-			test(files, err, stdout, stderr, callback);
+exec(`rm errors/*; make -C ../asm; ${c.map(x => `../asm/asm ../asm/mine/${x.replace('.cor', '.s')}`).join(';')}; make fclean -C ../asm`, () => {
+	exec(`make`, (err, stdout, stderr) => {
+		if (stderr.length)
+			return (console.log(stderr));
+		async.eachLimit(t, 4, function(files, callback) {
+			exec(`./corewar ../asm/mine/${files[0]} ../asm/mine/${files[1]}`,
+			(err, stdout, stderr) => {
+				test(files, err, stdout, stderr, callback);
+			});
+		}, function(err) {
 		});
-	}, function(err) {
 	});
 });
-/*var async = require("async");
-var c = ['ultima.cor',
-  'slider2.cor',
-  'Misaka_Mikoto.cor',
-  'champ.cor',
-  '_.cor',
-  'valid.cor'];
-var fs = require('fs');
-const {exec} = require('child_process');
-var t = [];
-c.forEach(x => c.forEach(y => t.push([x, y, t.length])));
-if (process.argv.length == 4 && !isNaN(process.argv[2] * 1) && !isNaN(process.argv[3] * 1))
-	t = t.slice(process.argv[2] * 1, process.argv[3] * 1);
-path = require("path");
-var F = 0;
-var P = 0;
-var show_msg = (arr) => {
-	console.log('_______________________________________________________________');
-	arr.forEach(x => console.log(x));
-}
-var fail = (arr, filename, id) =>
-{
-	show_msg(arr);
-	console.log(`Test nº ${id}:	\x1b[31mFAILING ${F++ +1}/${t.length} tests\x1b[0m`, filename);
-};
-var pass = (arr, filename, id) =>
-{
-	show_msg(arr);
-	console.log(`Test nº ${id}:	\x1b[32mPASSING ${P++ +1}/${t.length} tests\x1b[0m`, filename);
-};
-var test = (files, err, stdout, stderr, callback) => {
-	var zaz = fs.readFileSync(`zaz/${files[0]}_${files[1]}`).toString();
-	var z = 0;
-	var vburidar = stdout.split('\n');
-	var msg_arr = [`./corewar ../asm/mine/${files[0]} ../asm/mine/${files[1]}`];
-	if (!zaz.length)
-	{
-		msg_arr.push("Zaz vm probably returned an error");
-	}
-	else
-	{
-		zaz = zaz.split('\n');
-		var first_diff = -1;
-		var n_diff = 0;
-		for (var i = 0; i < zaz.length && i < vburidar.length; i++)
-		{
-			if (zaz[i] != vburidar[i])
-			{
-				n_diff++;
-				if (first_diff == -1)
-				{
-					msg_arr.push(`First diff at line ${i}:`);
-					msg_arr.push(`Zaz:	${zaz[i]}`);
-					msg_arr.push(`Us:	${vburidar[i]}`);
-					first_diff = i;
-				}
-			}
-		}
-		if (n_diff)
-			msg_arr.push(`${n_diff} different lines(before any potential crash)`);
-		if (zaz.length < vburidar.length)
-			msg_arr.push(`${vburidar.length - zaz.length} extraneous lines`);
-	}
-	if (stderr.length)
-	{
-		fs.writeFileSync(`errors/${files[0]}_${files[1]}.error`, stderr);
-		msg_arr.push(`\x1b[31mAn error was detected and stored in errors/${files[0]}_${files[1]}.error\x1b[0m`);
-	}
-	else if (vburidar.length < zaz.length)
-		msg_arr.push(`${zaz.length - vburidar.length} missing lines`);
-	if ((n_diff && !z) || stderr.length)
-		fail(msg_arr, `${files[0]} vs ${files[1]}`, files[2]);
-	else
-		pass(msg_arr, `${files[0]} vs ${files[1]}`, files[2]);
-	callback();
-};
 
-exec(`make; rm errors/*; make -C ../asm; ${c.map(x => `../asm/asm ../asm/mine/${x.replace('.cor', '.s')}`).join(';')}; make fclean -C ../asm`, () => {
-	async.eachLimit(t, 4, function(files, callback) {
-		exec(`./corewar ../asm/mine/${files[0]} ../asm/mine/${files[1]}`,
-		(err, stdout, stderr) => {
-			test(files, err, stdout, stderr, callback);
-		});
-	}, function(err) {
-	});
-});
-*/
