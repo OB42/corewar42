17468a13634aac980ff860e8d873be46c38f4763
Victor BURIDARD <vburidar@e3r12p19.42.fr>
2018-03-17 13:52:06 +0100

    modification ft_sti direct sur 2 octet entre -32768 et +32768

--- vm/ft_sti.c
+++ vm/ft_sti.c
@@ -6,7 +6,7 @@
 /*   By: vburidar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/07 19:09:01 by vburidar          #+#    #+#             */
-/*   Updated: 2018/03/17 12:50:54 by vburidar         ###   ########.fr       */
+/*   Updated: 2018/03/17 13:51:32 by vburidar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,13 @@
 
 void	ft_print_sti(t_proc *proc, int val_1, int val_2)
 {
-	if (proc->ins->tab[1].type != 3)
-		val_1 = ft_decal(proc->init, proc->curseur, proc->ins->tab[1].val_type);
-	if (proc->ins->tab[2].type != 3)
-		val_2 = ft_decal(proc->init, proc->curseur, proc->ins->tab[2].val_type);
 	if (ft_val_ocp(proc->ins->ocp, 0) == 1 && proc->success == 1)
 	{
 		ft_printf("P%5d | %s", proc->id, proc->ins->name);
 		ft_printf(" r%d %d %d", proc->ins->param[0], val_1, val_2);
-		if (val_1 + val_2 > 0)
-			ft_printf("\n       | -> store to %d + %d = %d (with pc and mod %d)\n",
-					val_1, val_2, val_1 + val_2,
-					proc->curseur - proc->init + (val_1 + val_2) % IDX_MOD);
-		else
-			ft_printf("\n       | -> store to %d + %d = %d (with pc and mod %d)\n",
-					val_1, val_2, val_1 + val_2,
-					proc->curseur - proc->init + -((-val_1 - val_2) % IDX_MOD));
+		ft_printf("\n       | -> store to %d + %d = %d (with pc and mod %d)\n",
+				val_1, val_2, val_1 + val_2,
+				proc->curseur - proc->init + (val_1 + val_2) % IDX_MOD);
 	}
 	ft_print_instru(proc);
 }
@@ -48,10 +39,6 @@ void	ft_sti(t_ins *ins, t_proc *proc)
 	{
 		val_1 = ins->tab[1].val_type;
 		val_2 = ins->tab[2].val_type;
-		if (proc->ins->tab[1].type != 3)
-			val_1 = ft_decal(proc->init, proc->curseur, proc->ins->tab[1].val_type);
-		if (proc->ins->tab[2].type != 3)
-			val_2 = ft_decal(proc->init, proc->curseur, proc->ins->tab[2].val_type);
 		tmp = ft_oob(proc->init, proc->curseur + (val_1 + val_2) % IDX_MOD);
 		ft_write_ram(proc->reg[ins->param[0]], 4, tmp, proc);
 	}
