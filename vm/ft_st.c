91c00391626e59bb3994ef3250d3009b398e1422
Victor BURIDARD <vburidar@e3r12p19.42.fr>
2018-03-17 15:13:25 +0100

    debug affichage st -> DIR entre -32768 et +32768

--- vm/ft_st.c
+++ vm/ft_st.c
@@ -6,7 +6,7 @@
 /*   By: vburidar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/07 15:48:38 by vburidar          #+#    #+#             */
-/*   Updated: 2018/03/17 14:07:51 by vburidar         ###   ########.fr       */
+/*   Updated: 2018/03/17 15:11:57 by vburidar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,18 @@
 
 void	ft_print_st(t_proc *proc)
 {
+	int value;
+
+	value = proc->ins->param[1];
+	if (value > 32768)
+		value -= 65536;
 	if (proc->ins->ocp == 80 || proc->ins->ocp == 112)
 	{
 		ft_printf("P%5d | %s", proc->id, proc->ins->name);
-		ft_printf(" r%d %d", proc->ins->param[0], proc->ins->param[1]);
+		if (proc->ins->tab[1].type == 1)
+			ft_printf(" r%d %d", proc->ins->param[0], proc->ins->param[1]);
+		else
+			ft_printf(" r%d %d", proc->ins->param[0], value);	
 		ft_printf("\n");
 	}
 	else
