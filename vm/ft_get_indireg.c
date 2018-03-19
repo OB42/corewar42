fc42279dbe1a8b5702b4b9bd0fe7b6667b841f33
Victor BURIDARD <vburidar@e2r12p1.42.fr>
2018-03-19 19:46:51 +0100

    modification des instructions

--- vm/ft_get_indireg.c
+++ vm/ft_get_indireg.c
@@ -6,7 +6,7 @@
 /*   By: vburidar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/06 20:54:24 by vburidar          #+#    #+#             */
-/*   Updated: 2018/03/19 16:36:18 by vburidar         ###   ########.fr       */
+/*   Updated: 2018/03/19 19:37:45 by vburidar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,10 @@ unsigned char	*ft_get_ind(t_ins *ins, unsigned char *curseur, int n_param,
 	ins->tab[n_param].val_type = 256 * *(curseur) + *(curseur + 1);
 	ins->param[n_param] = 256 * *(curseur) + *(curseur + 1);
 	tmp = ft_oob(init, curseur + ins->tab[n_param].val_type - ins->size - 2  + 1);
-	ins->tab[n_param].val_type = ft_get_int(tmp, 4);
+	if (ft_strcmp(ins->name, "lld") == 0)
+		ins->tab[n_param].val_type = ft_get_int(tmp, 2);
+	else	
+		ins->tab[n_param].val_type = ft_get_int(tmp, 4);
 	ins->size = ins->size + 2;
 	return(ft_oob(init, curseur + 2));
 }
@@ -60,13 +63,14 @@ unsigned char	*ft_get_dir(t_ins *ins, unsigned char *curseur, int n_param,
 unsigned char	*ft_get_reg(t_proc *proc, unsigned char *curseur, int n_param,
 		unsigned char *init)
 {
-	proc->ins->param[n_param] = *ft_oob(proc->init, curseur);
-	if (proc->ins->param[n_param] == 0)
-		ft_print_nxt(proc->init, curseur, 10);
+	if (curseur - proc->init > 4095)
+		proc->ins->param[n_param] = *ft_oob(proc->init, curseur);
+	else
+		proc->ins->param[n_param] = *curseur;
 	if (proc->ins->param[n_param] < REG_NUMBER + 1)
 		proc->ins->tab[n_param].val_type = proc->reg[proc->ins->param[n_param]];
 	else
-		(proc->ins->fail = 1);
+		proc->ins->fail = 1;
 	proc->ins->tab[n_param].type = 1;
 	proc->ins->size = proc->ins->size + 1;
 	return (ft_oob(init, curseur + 1));
