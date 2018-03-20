bcbd3413334e981cd03578bf33297adda73812ab
Otmane BENAZZOU <obenazzo@e3r12p20.42.fr>
2018-03-20 20:28:42 +0100

    Defining last_live_id

--- vm/ft_live.c
+++ vm/ft_live.c
@@ -24,8 +24,11 @@ void	ft_print_live(t_proc *proc)
 	if (proc->ins->param[0] > 2147483647)
 		proc->ins->tab[0].val_type -= 4294967295;
 	if (ft_abs(param) <= proc->corewar->nb_champ && param < 0)
+	{
 		ft_printf("Player %d (%s) is said to be alive\n",
 			-param, proc->corewar->tab_champ[-param - 1].header.prog_name);
+		proc->corewar->last_live_id = -param - 1;
+	}
 	ft_print_instru(proc);
 }
 
