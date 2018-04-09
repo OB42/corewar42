1ce532eb37347ac4406f3f34d02e75d1ef7cd2c5
OB42 <19638906+OB42@users.noreply.github.com>
2018-04-09 22:28:30 +0200

    fixing some memory leaks

--- vm/ft_del.c
+++ vm/ft_del.c
@@ -16,15 +16,23 @@
 t_proc	*ft_del(t_proc *lst_proc)
 {
 	t_proc *curseur;
+	void *workaround;
 
 	curseur = lst_proc;
 	while (curseur->nxt->id != lst_proc->id)
 		curseur = curseur->nxt;
 	if (curseur == lst_proc)
 	{
+		pr_free(lst_proc->ins);
 		pr_free(lst_proc);
 		return (NULL);
 	}
-	curseur->nxt = lst_proc->nxt;
+	workaround = lst_proc->nxt;
+	if (curseur->nxt)
+	{
+		pr_free(curseur->nxt->ins);
+		pr_free(curseur->nxt);
+	}
+	curseur->nxt = workaround;
 	return (curseur->nxt);
 }
